9a1d27873ffe2af5a22becc6a4d5652be1f415c3
Ming Wang <miwan@redhat.com>
2024-07-03 15:25:12 -0400

    clean

--- src/main/java/io/cryostat/agent/MainModule.java
+++ src/main/java/io/cryostat/agent/MainModule.java
@@ -15,7 +15,6 @@
  */
 package io.cryostat.agent;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -146,7 +145,7 @@ public abstract class MainModule {
         try {
             if (!trustAll) {
                 SSLContext sslCtx = SSLContext.getInstance(clientTlsVersion);
-
+                /**
                 // initialize truststore
                 KeyStore ts = KeyStore.getInstance(KeyStore.getDefaultType());
                 ts.load(null, null);
@@ -162,6 +161,7 @@ public abstract class MainModule {
 
                 // set up HTTPS context
                 sslCtx.init(null, tmf.getTrustManagers(), null);
+                */
                 return sslCtx;
             }
 
